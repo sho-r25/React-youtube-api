ba2701c245a6c16a19b47842c0c30a1295408ab1
sho-r25 <tori.ushi12@gmail.com>
2021-01-08 03:45:27 +0900

    first commit

---
+++ src/pages/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+
+const Search = () => {
+  return (
+    <Layout>
+      search page
+    </Layout>
+  )
+}
+
+export default Search
+
