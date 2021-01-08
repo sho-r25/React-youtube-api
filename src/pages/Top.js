0125163a40508df51bca2aa38b262534c8c0d382
sho-r25 <tori.ushi12@gmail.com>
2021-01-08 19:27:59 +0900

    リスト表示作成

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React, { useEffect, useContext } from "react";
+import Layout from "../components/Layout/Layout";
+import { fetchPopularData } from "../api/index";
+import { Store } from "../store/index";
+import VideoGrid from "../components/VideoGrid/VideoGrid";
+import VideoGridItem from "../components/VideoGridItem/VideoGridItem";
 
 const Top = () => {
+  const { globalState, setGlobalState } = useContext(Store);
+
+  useEffect(() => {
+    fetchPopularData().then((res) => {
+      console.log("data", res);
+      setGlobalState({
+        type: "SET_POPULAR",
+        payload: { popular: res.data.items },
+      });
+    });
+    // eslint-disable-next-line
+  }, []);
   return (
-    <Layout>
-      top page
-    </Layout>
+  <Layout>
+    <VideoGrid>
+      {
+        globalState.popular && globalState.popular.map((popular)=>{
+          return (
+            <VideoGridItem
+            id={popular.id}
+            key={popular.id}
+            src={popular.snippet.thumbnails.standard.url}
+            title={popular.snippet.title} />
+          )
+        })
+      }
+    </VideoGrid>
+  </Layout>
   )
-}
+};
 
-export default Top
-
+export default Top;
